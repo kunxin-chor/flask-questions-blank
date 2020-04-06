5ceec750e586cf7242fcec824e178064524c5bae
kunxin-chor <chorkunxin@yahoo.com>
2020-04-06 02:38:24 +0000

    Added solutions for q1 to q4

--- 04-basic-templates/app.py
+++ 04-basic-templates/app.py
@@ -3,6 +3,18 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    # since are all templates must be in templates folder, there is no need for us
+    # to put the ./templates in front. Flask will know that we are supposed to
+    # get the templates from the ./templates folder
+    #return render_template('./templates/index.template.html') --> this is wrong
+    return render_template('index.template.html', message='goodbye cruel world')
+
+@app.route('/gallery')
+def show_gallery():
+    return render_template('gallery.template.html')
+
 # "magic code" -- boilerplate
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
